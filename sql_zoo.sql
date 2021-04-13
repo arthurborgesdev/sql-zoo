014be78def763b903711834f593e8f41b2a5d8d4
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-13 17:45:09 -0300

    Add all exercises of 5 - SUM AND COUNT and USING GROUP AND HAVING

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -284,3 +284,62 @@ SELECT SUM(population)
   FROM world
 
 -- 5.2
+SELECT DISTINCT continent
+  FROM world
+
+-- 5.3
+SELECT SUM(gdp)
+  FROM world
+ WHERE continent = 'Africa'
+
+-- 5.4
+SELECT COUNT(name)
+  FROM world
+ WHERE area > 1000000
+
+-- 5.5
+SELECT SUM(population)
+  FROM world
+ WHERE name IN('Estonia', 'Latvia', 'Lithuania')
+
+
+-- (USING GROUP BY AND HAVING)
+-- extra 1
+SELECT continent, COUNT(name)
+  FROM world
+ GROUP BY continent
+
+-- extra 2
+SELECT continent, SUM(population)
+  FROM world
+ GROUP BY continent
+
+-- extra 3
+SELECT continent, COUNT(name)
+  FROM world
+ WHERE population>200000000
+ GROUP BY continent
+
+-- extra 4
+SELECT continent, SUM(population)
+  FROM world
+ GROUP BY continent
+HAVING SUM(population)>500000000
+
+
+-- 5.6
+SELECT continent, COUNT(name)
+  FROM world
+ GROUP BY continent
+
+-- 5.7
+SELECT continent, COUNT(name)
+  FROM world
+ WHERE population > 10000000
+ GROUP BY continent
+
+-- 5.8
+SELECT continent
+  FROM world
+ GROUP BY continent
+ HAVING SUM(population) > 100000000
